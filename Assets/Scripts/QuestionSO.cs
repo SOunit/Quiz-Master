9352aeb029d99fc66b4e1940b08067dc5de08d77
SOUnit <workbookwork7@gmail.com>
2022-11-03 12:58:32 -0700

    add text and array to question-scriptable-object

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -8,4 +8,25 @@ public class QuestionSO : ScriptableObject
     [TextArea(2, 6)]
     [SerializeField]
     string question = "Enter new question text here";
+
+    [SerializeField]
+    string[] answers = new string[4];
+
+    [SerializeField]
+    int correctAnswerIndex = 0;
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
 }
