f1238457dd719a7d09c70278d7c91d2b85e8379a
SOUnit <workbookwork7@gmail.com>
2022-11-06 08:41:56 -0800

    add timer fraction

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -10,7 +10,12 @@ public class Timer : MonoBehaviour
     [SerializeField]
     float timeToShowCorrectAnswer = 10f;
 
+    //  public is not good approach but this game is small so use public here
+    public bool loadNextQuestion;
+
     public bool isAnsweringQuestion = false;
+
+    public float fillFraction;
 
     float timerValue;
 
@@ -19,13 +24,25 @@ public class Timer : MonoBehaviour
         updateTimer();
     }
 
+    public void CancelTimer()
+    {
+        timerValue = 0;
+    }
+
     void updateTimer()
     {
         timerValue -= Time.deltaTime;
 
         if (isAnsweringQuestion)
         {
-            if (timerValue <= 0)
+            if (timerValue > 0)
+            {
+                // 0 / 10 = 0
+                // 5 / 10 = 0.5
+                // 10 / 10 = 1
+                fillFraction = timerValue / timeToCompleteQuestion;
+            }
+            else
             {
                 timerValue = timeToShowCorrectAnswer;
                 isAnsweringQuestion = false;
@@ -33,11 +50,23 @@ public class Timer : MonoBehaviour
         }
         else
         {
-            if (timerValue <= 0)
+            if (timerValue > 0)
+            {
+                fillFraction = timerValue / timeToShowCorrectAnswer;
+            }
+            else
             {
                 timerValue = timeToCompleteQuestion;
                 isAnsweringQuestion = true;
+                loadNextQuestion = true;
             }
         }
+
+        Debug
+            .Log(isAnsweringQuestion +
+            " : " +
+            timerValue +
+            " : " +
+            fillFraction);
     }
 }
